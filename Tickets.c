88ae41cd5a13d6eccea630a000b91fde3aea95e8
Chadikabhanu <147791849+Chadikabhanu@users.noreply.github.com>
2023-11-11 09:44:29 +0530

    #include<stdio.h> int main() {     int x,t;     scanf("%d",&x);     t=4*x;     if(t<=1000)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Tickets.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x,t;
+    scanf("%d",&x);
+    t=4*x;
+    if(t<=1000)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
