83d1b46e511516306cfd2dc85d7b822c650286b4
Chadikabhanu <147791849+Chadikabhanu@users.noreply.github.com>
2023-11-12 13:13:07 +0530

    #include<stdio.h> #include<math.h> int main() {     int x1,x2,y1,y2;     scanf("%d%d%d%d",&x1,&y1,&x2,&y2);     if((x1+y1)>=(x2+y2))     {         printf("%d",(x2+y2));     }     else     {         printf("%d",(x1+y1));     }
    }

---
+++ Interior_Design.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int x1,x2,y1,y2;
+    scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
+    if((x1+y1)>=(x2+y2))
+    {
+        printf("%d",(x2+y2));
+    }
+    else
+    {
+        printf("%d",(x1+y1));
+    }
+    
+}
