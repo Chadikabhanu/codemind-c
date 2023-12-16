22eb3acb7e50afd6249dbae9a78197674c19da5f
Chadikabhanu <147791849+Chadikabhanu@users.noreply.github.com>
2023-12-16 09:56:57 +0530

    #include<stdio.h> int main() {     int n,max;     scanf("%d",&n);     int arr[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     max=arr[0];     for(i=1;i<n;i++)     {         if(max<arr[i])         {             max=arr[i];         }     }     printf("%d",max); }

---
+++ Maximum_of_an_Array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,max;
+    scanf("%d",&n);
+    int arr[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    max=arr[0];
+    for(i=1;i<n;i++)
+    {
+        if(max<arr[i])
+        {
+            max=arr[i];
+        }
+    }
+    printf("%d",max);
+}
